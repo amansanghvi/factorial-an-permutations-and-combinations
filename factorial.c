400ea3b2587f8ef61ce07a39f3d3508027875279
Aman Sanghvi <aman302@hotmail.co.uk>
2016-06-03 22:18:16 +1000

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -4,8 +4,16 @@
 int factorial(int num1);
 int permutation(int num1, int num2);
 int combination(int num1, int num2);
+void testFactorial();
+void testPermutations();
+void testCombinations();
 
 int main (int argc,char * argv[]) {
+    
+    testFactorial();
+    testPermutations();
+    testCombinations();
+
     char choice = 'F';
     printf("Factorial, Permutation or Combination?"
       "\n Enter 'F', 'P' or 'C':");
@@ -31,14 +39,18 @@ int main (int argc,char * argv[]) {
 }
 
 int factorial(int num1){
-     int result = 0;
-     int counter = num1-1;
+     result = 1;
+     if (num1 != 0) {
+        int counter = num1-1;
 
-     while (counter >= 1) {
-       num1 = num1*counter;
-       counter --;
+         while (counter >= 1) {
+           num1 = num1*counter;
+           counter --;
+         }
+         result = num1;
+     } else {
+         result = 1;
      }
-     result = num1;
      return result;
 }
 
@@ -72,4 +84,50 @@ int combination(int num1, int num2){
 
      result = num1/num2;
      return result;
+}
+void testFactorial {
+ 
+    assert(factorial(0) == 1);
+    assert(factorial(1) == 1);
+    assert(factorial(2) == 2);
+    assert(factorial(3) == 6);
+    assert(factorial(4) == 24);
+    assert(factorial(5) == 120);
+    assert(factorial(6) == 720);
+    assert(factorial(7) == 5040);
+    assert(factorial(8) == 40320);
+    assert(factorial(9) == 362880);
+
+    printf("Factorial tests Passed!\n");    
+}
+void testPermutations {
+
+    assert(permutation(1, 1) == 1);
+    assert(permutation(10, 5) == 30240);
+    assert(permutation(7, 5) == 2520);
+    assert(permutation(9, 3) == 504);
+    assert(permutation(5, 5) == 120);
+    assert(permutation(11, 5) == 55440);
+    assert(permutation(51, 2) == 2550);
+    assert(permutation(12, 4) == 11880);
+    assert(permuation(6, 4) == 360);
+    assert(permutaion(101, 3) == 999900);    
+    
+    printf("Perms tests Passed!\n");    
+}
+
+void testCombinations {
+    
+    assert(combination(1, 1) == 1);
+    assert(combination(10, 5) == 252);
+    assert(combination(21, 8) == 319770);
+    assert(combination(9, 3) == 84);
+    assert(combination(55, 55) == 1);
+    assert(combination(12, 6) == 924);
+    assert(combination(18, 9) == 48620);
+    assert(combination(12, 4) == 495);
+    assert(combination(6, 5) == 6);
+    assert(combination(101, 3) == 16650);    
+    
+    printf("Combs tests Passed!\n");    
 }
